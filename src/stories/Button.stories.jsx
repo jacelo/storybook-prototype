84f05ffd172f5872b2007e0eee52fcb7c3dd9c77
Joe Bergantine <joe@kinsa.us>
2023-02-13 16:03:01 +0000

    first commit

---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import { userEvent, within } from '@storybook/testing-library';
+
+import { Button } from './Button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const PrimaryWithFocusState = Template.bind({});
+PrimaryWithFocusState.args = {
+  primary: true,
+  label: 'Button',
+};
+PrimaryWithFocusState.play = async ({ canvasElement }) => {
+  // Starts querying the component from its root
+  const canvas = within(canvasElement);
+
+  // Looks up the button and interacts with it.
+  await canvas.getByRole('button').focus();
+};
+
+export const PrimaryWithHoverState = Template.bind({});
+PrimaryWithHoverState.args = {
+  primary: true,
+  label: 'Button',
+};
+PrimaryWithHoverState.parameters = {
+  pseudo: { hover: true }
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
